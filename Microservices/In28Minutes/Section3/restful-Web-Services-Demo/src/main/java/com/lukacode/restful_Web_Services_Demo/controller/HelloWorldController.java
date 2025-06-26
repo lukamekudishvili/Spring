ed8a8ece18b63867d223f4b7b51b531108c5919e
lukamekudishvili <116336769+lukamekudishvili@users.noreply.github.com>
2025-06-26 11:24:16 +0400

    rest

--- Microservices/In28Minutes/Section3/restful-Web-Services-Demo/src/main/java/com/lukacode/restful_Web_Services_Demo/controller/HelloWorldController.java
+++ Microservices/In28Minutes/Section3/restful-Web-Services-Demo/src/main/java/com/lukacode/restful_Web_Services_Demo/controller/HelloWorldController.java
@@ -1,15 +1,25 @@
 package com.lukacode.restful_Web_Services_Demo.controller;
 
 import com.lukacode.restful_Web_Services_Demo.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 @RequestMapping("/api")
 public class HelloWorldController {
+    private final MessageSource messageSource;
 
+    @Autowired
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello")
     public HelloWorldBean greeting(){
@@ -19,6 +29,12 @@ public class HelloWorldController {
     @GetMapping("/hello/{name}")
     public HelloWorldBean greeting(@PathVariable("name") String name){
         return new HelloWorldBean("Hello world "+name);
+    }
+
+    @GetMapping(value = "/hello-i18n" ,produces = "text/plain;charset=UTF-8")
+    public String helloWorldI18N(){
+        Locale locale= LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default greeting", locale);
     }
 
 
