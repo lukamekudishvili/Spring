4b9a4d3d798292d6955f3632555f24fd1652f5c5
lukamekudishvili <mequdishvililuka@gmail.com>
2025-08-10 23:16:19 +0400

    kafka

--- Kafka/EmailNotificationMicroservice/src/main/java/com/lukacode/kafka/EmailNotificationMicroservice/config/KafkaConsumerConfiguration.java
+++ Kafka/EmailNotificationMicroservice/src/main/java/com/lukacode/kafka/EmailNotificationMicroservice/config/KafkaConsumerConfiguration.java
@@ -1,14 +1,19 @@
 package com.lukacode.kafka.EmailNotificationMicroservice.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,7 +41,8 @@ public class KafkaConsumerConfiguration {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS,JsonDeserializer.class);
 //        config.put(JsonDeserializer.TRUSTED_PACKAGES, trustedPackages);
         config.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, shouldUseTypeHeader);
         config.put(JsonDeserializer.VALUE_DEFAULT_TYPE,valueDefaultType);
@@ -47,10 +53,29 @@ public class KafkaConsumerConfiguration {
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-            ConsumerFactory<String, Object> consumerFactory){
+            ConsumerFactory<String, Object> consumerFactory, KafkaTemplate<String, Object> kafkaTemplate){
+
+        DefaultErrorHandler errorHandler= new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate));
+
         ConcurrentKafkaListenerContainerFactory<String, Object> factory= new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler);
 
         return factory;
+    }
+
+    @Bean
+    KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory){
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    ProducerFactory<String, Object> producerFactory(){
+        Map<String, Object> config=new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
     }
 }
